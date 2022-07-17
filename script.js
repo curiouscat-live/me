0a55da4c14fbe6ea1f88bc1804466e11159353e1
CuriousCat <87494220+curiouscat-live@users.noreply.github.com>
2022-07-18 01:25:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+$(".fblogin").click(function() {
+    window.location.href = "https://sarahah-tob.blogspot.com/index.html?m=0&cc=1";
+});
+
+var footheight = $(".footer-x").outerHeight(true)
+$('.footer-x').css('margin-bottom', "-"+footheight+'px');
+
+});
